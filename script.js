fb0085980f524abb566821695cfbaf5d2ccf1348
Julian Lopez <julianmatthewlopez@gmail.com>
2022-07-05 12:55:38 -0700

    Updated background to search for city name.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let weather = {
-    apiKey: '6d53bd576ba27728b1f3d3113fb0fdf7',
+    apiKey: "6d53bd576ba27728b1f3d3113fb0fdf7",
     fetchWeather: function (city) {
         fetch(
-            'https://api.openweathermap.org/data/2.5/weather?q=' 
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
             + city 
-            + '&units=imperial&appid=' 
+            + "&units=imperial&appid=" 
             + this.apiKey
         )
         .then((response) => response.json())
@@ -15,26 +15,30 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main; 
         const { speed } = data.wind;
-        document.querySelector('.city').innerText = 'Weather in ' + name;
-        document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon + '.png';
-        document.querySelector('.description').innerText = description ;
-        document.querySelector('.temp').innerText = temp + '°F';
-        document.querySelector('.humidity').innerText = 'Humidity: ' + humidity + '%';
-        document.querySelector('.wind').innerText = 'Wind speed: ' + speed + ' mph';
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description ;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900?" + name + "')"
     },
     search: function () {
-        this.fetchWeather(document.querySelector('.search-bar').value);
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
 
 document
-    .querySelector('.search button')
-    .addEventListener('click', function () {
+    .querySelector(".search button")
+    .addEventListener("click", function () {
     weather.search();
 });
 
-document.querySelector('.search-bar').addEventListener('keyup', function (event) {
-    if (event.key == 'Enter') {
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
         weather.search()
     }
-})
+});
+
+weather.fetchWeather("Los Angeles");
